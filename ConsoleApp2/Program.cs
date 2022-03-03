58a666f5d706e9460d92d9de74742068ff1c3f62
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-03 13:16:43 +0000

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String text, reverseText = "";
+
+            Console.Write("Please enter some text: ");
+            text = Console.ReadLine();
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reverseText = reverseText + text[i];
+            }
+
+
+            Console.Write("Here is your text in reverse order: " + reverseText);
+            Console.Read();
+        }
+    }
+}
